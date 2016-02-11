4481e8c733d5fa2c9c1373610e1472965e64a145
Aaron Robb <airrobb@gmail.com>
2016-02-11 22:00:40 +1300

    time reducer tests working

--- src/reducer.js
+++ src/reducer.js
@@ -10,24 +10,35 @@ const initialState = Map({
 })
 
 function next(state) {
-  const nextHour = state.get('time').get('hour') + 1
-  return state
+  return nextHour(state)
 }
 
 function nextHour(state) {
-
+  const nextTime = state.getIn(['time', 'hour']) + 1
+  return nextTime === 22 ?
+    nextDay(state.setIn(['time', 'hour'], 7)) :
+    state.setIn(['time', 'hour'], nextTime)
 }
 
 function nextDay(state) {
-
+  const nextTime = state.getIn(['time', 'day']) + 1
+  return nextTime === 6 ?
+     nextWeek(state.setIn(['time', 'day'], 1)) :
+     state.setIn(['time', 'day'], nextTime)
 }
 
 function nextWeek(state) {
-
+  const nextTime = state.getIn(['time', 'week']) + 1
+  return nextTime === 4 ?
+     nextPhase(state.setIn(['time', 'week'], 1)) :
+     state.setIn(['time', 'week'], nextTime)
 }
 
 function nextPhase(state) {
-
+  const nextTime = state.getIn(['time', 'phase']) + 1
+  return nextTime === 4 ?
+    state.set('gameover', true) :
+     state.setIn(['time', 'phase'], nextTime)
 }
 
 export default function(state = initialState, action) {
@@ -37,7 +48,7 @@ export default function(state = initialState, action) {
     case 'END_GAME':
       return state
     case 'NEXT':
-      return state
+      return next(state)
     case 'CHANGE_ACTION':
       return state
     case 'BUY_ITEM':
--- test/reducer-spec.js
+++ test/reducer-spec.js
@@ -3,25 +3,107 @@ import { List, Map, fromJS } from 'immutable'
 
 import reducer from '../src/reducer.js'
 
+
+
 describe('reducer', () => {
   describe('NEXT', () => {
     describe('hours', () => {
-      it('increases the hour by 1')
+      it('increases the hour', () => {
+        const initialState = Map({
+          time: Map({
+            hour: 7,
+            day: 1,
+            week: 1,
+            phase: 1
+          })
+        })
+        const action = {
+          type: 'NEXT'
+        }
+        const nextState = reducer(initialState, action)
+        expect(nextState.getIn(['time', 'hour'])).to.equal(8)
+      })
       describe('active actions', () => {
         //TODO write each actions hourly modifier test
+
+
       })
     })
     describe('days', () => {
-      it('increases the day by 1')
+      it('increases the day', () => {
+        const initialState = Map({
+          time: Map({
+            hour: 21,
+            day: 1,
+            week: 1,
+            phase: 1
+          })
+        })
+        const action = {
+          type: 'NEXT'
+        }
+        const nextState = reducer(initialState, action)
+        expect(nextState.getIn(['time', 'hour'])).to.equal(7)
+        expect(nextState.getIn(['time', 'day'])).to.equal(2)
+      })
       //TODO write day recharge effect tests
     })
     describe('weeks', () => {
-      it('increase the week by 1')
+      it('increase the week', () => {
+        const initialState = Map({
+          time: Map({
+            hour: 21,
+            day: 5,
+            week: 1,
+            phase: 1
+          })
+        })
+        const action = {
+          type: 'NEXT'
+        }
+        const nextState = reducer(initialState, action)
+        expect(nextState.getIn(['time', 'hour'])).to.equal(7)
+        expect(nextState.getIn(['time', 'day'])).to.equal(1)
+        expect(nextState.getIn(['time', 'week'])).to.equal(2)
+
+      })
       //TODO write week recharge effect tests
     })
     describe('phases', () => {
-      it('increases the phase by 1')
-      //TODO write phase recharge effect tests
+      it('increases the phase', () => {
+        const initialState = Map({
+          time: Map({
+            hour: 21,
+            day: 5,
+            week: 3,
+            phase: 1
+          })
+        })
+        const action = {
+          type: 'NEXT'
+        }
+        const nextState = reducer(initialState, action)
+        expect(nextState.getIn(['time', 'hour'])).to.equal(7)
+        expect(nextState.getIn(['time', 'day'])).to.equal(1)
+        expect(nextState.getIn(['time', 'week'])).to.equal(1)
+        expect(nextState.getIn(['time', 'phase'])).to.equal(2)
+      })
+      //TODO write phase recharge effect tests'
+      it('ends the game', () => {
+        const initialState = Map({
+          time: Map({
+            hour: 21,
+            day: 5,
+            week: 3,
+            phase: 3
+          })
+        })
+        const action = {
+          type: 'NEXT'
+        }
+        const nextState = reducer(initialState, action)
+        expect(nextState.get('gameover')).to.be.true
+      })
     })
   })
   describe('CHANGE_ACTION', () => {
