8c07827a0625ebe59ab4916daec8aea6329b8744
NiluK <nkulasingham@gmail.com>
2016-02-13 12:47:26 +1300

    all tests passing

--- src/reducer.js
+++ src/reducer.js
@@ -36,12 +36,12 @@ const initialState = Map({
     ])
 })
 
-function hasEnoughResources(state, task){
+function hasEnoughResources(state, task){ 
   const costs = state.toJS().tasks[task].initalCosts
   for (let j =0; j < Object.keys(costs).length;  j++) {
     const cost = costs[Object.keys(costs)[j]]
     const resource = state.toJS().resources[Object.keys(costs)[j]]
-    if (resource < cost) return false
+    if (resource <= cost) return false
   }
   return true
 }
@@ -49,14 +49,14 @@ function hasEnoughResources(state, task){
 function activateTask(state, task) {
   if (task === state.get('activeTask')) return state
   const taskId = state.toJS().tasks.findIndex((indexes) => indexes.name === task)
-  return hasEnoughResources(state, taskId) ? depleteResources(state, task, taskId) :
+  return hasEnoughResources(state, taskId) ? depleteResources(state, task, taskId) : 
   state
 }
 
 function continueTask (state, task) {
   const taskId = state.toJS().tasks.findIndex((indexes) => indexes.name === task)
-  return hasEnoughResources(state, taskId) ? undergoTask(state, task, taskId) :
-  state.set('gameover', true)
+  return hasEnoughResources(state, taskId) ? undergoTask(state, task, taskId) : 
+  state
 }
 
 function depleteResources (state, task, taskId) {
@@ -65,7 +65,7 @@ function depleteResources (state, task, taskId) {
   for (let i =0; i < Object.keys(costs).length;  i++) {
     const cost = costs[Object.keys(costs)[i]]
     const resource = state.toJS().resources[Object.keys(costs)[i]]
-    newState = newState.updateIn(['resources', Object.keys(costs)[i]],
+    newState = newState.updateIn(['resources', Object.keys(costs)[i]], 
       (resource) => resource-cost)
   }
   newState = newState.update('activeTask', (value) => value = `${task}`)
@@ -78,25 +78,25 @@ function undergoTask(state, task, taskId) {
   for (let i =0; i < Object.keys(costs).length;  i++) {
     const cost = costs[Object.keys(costs)[i]]
     const resource = state.toJS().resources[Object.keys(costs)[i]]
-    newState = newState.updateIn(['resources', Object.keys(costs)[i]],
+    newState = newState.updateIn(['resources', Object.keys(costs)[i]], 
       (resource) => resource+cost)
   }
   const gains = state.toJS().tasks[taskId].skills
   for (let i =0; i < Object.keys(gains).length;  i++) {
     const gain = gains[Object.keys(gains)[i]]
     const resource = state.toJS().skills[Object.keys(gains)[i]]
-    newState = newState.updateIn(['skills', Object.keys(gains)[i]],
+    newState = newState.updateIn(['skills', Object.keys(gains)[i]], 
       (resource) => resource+gain)
   }
   return fromJS(newState)
 }
 
-function validState(state) {
+function validState(state) { 
   const hour = state.getIn(['time', 'hour'])
   const day = state.getIn(['time', 'day'])
   const week = state.getIn(['time', 'week'])
   const phase = state.getIn(['time', 'phase'])
-  return (hour> 22 || hour < 7 || day > 6 ||  day < 1 ||
+  return (hour> 22 || hour < 7 || day > 6 ||  day < 1 || 
     week > 4 || week < 1 || phase > 4 || phase < 0) ? false : true
 }
 
@@ -106,26 +106,27 @@ function next(state) {
 
 
 function nextHour(state) {
-  const nextHour =  state.getIn(['time', 'hour']) + 1
-  return (nextHour > 21) ? nextDay(state.setIn(['time', 'hour'], 7)) :
+  const nextHour =  state.getIn(['time', 'hour']) + 1 
+  const nextState = continueTask(state, state.get('activeTask'))
+  return (nextHour > 21) ? nextDay(state.setIn(['time', 'hour'], 7)) : 
   state.setIn(['time', 'hour'], nextHour)
 }
 
 function nextDay (state) {
-  const nextDay = state.getIn(['time', 'day']) + 1
-  return nextDay > 5 ? nextWeek(state.setIn(['time', 'day'], 1)) :
+  const nextDay = state.getIn(['time', 'day']) + 1 
+  return nextDay > 5 ? nextWeek(state.setIn(['time', 'day'], 1)) : 
   state.setIn(['time', 'day'], nextDay)
 }
 
 function nextWeek (state) {
-  const nextWeek = state.getIn(['time', 'week']) + 1
-  return nextWeek > 3 ? nextPhase(state.setIn(['time', 'week'], 1)) :
+  const nextWeek = state.getIn(['time', 'week']) + 1 
+  return nextWeek > 3 ? nextPhase(state.setIn(['time', 'week'], 1)) : 
   state.setIn(['time', 'week'], nextWeek)
 }
 
 function nextPhase (state) {
-  const nextPhase = state.getIn(['time', 'phase']) + 1
-  return nextPhase > 3 ? state.set('gameover', true) :
+  const nextPhase = state.getIn(['time', 'phase']) + 1 
+  return nextPhase > 3 ? state.set('gameover', true) : 
   state.setIn(['time', 'phase'], nextPhase)
 }
 
--- test/reducer-spec.js
+++ test/reducer-spec.js
@@ -10,22 +10,118 @@ describe('reducer', () => {
     }
     describe('Invalid State', () => {
       it('sets the game to be over', () => {
-        const initialState = Map({time: Map({hour: 6634, day: 1, week: 1,
-          phase: 1})})
+        const initialState = Map({
+          activeTask: 'Pair Programming',
+          time: Map({
+            hour: 6634,
+            day: 1,
+            week: 1,
+            phase: 1
+          }),
+          resources: Map({
+            energy: 500,
+            mood: 100
+          }),
+          skills: Map({
+            softSkills: 0,
+            techSkills: 0
+          }),
+          tasks: List([
+            Map({
+              name: 'Pair Programming',
+              resources: Map({
+                energy: -10,
+                mood: -3
+              }),
+              skills: Map({
+                softSkills: 3,
+                techSkills: 1
+              }),
+              initalCosts: Map({
+                energy: 10,
+                mood: 3
+              })
+            })
+            ])
+        })
         const nextState = reducer(initialState, action)
         expect(nextState.get('gameover')).to.equal(true)
       })
     })
     describe('hours', () => {
       it('increases the hour', () => {
-        const initialState = Map({time: Map({hour: 7, day: 1, week: 1,
-         phase: 1})})
+        const initialState = Map({
+          activeTask: 'Pair Programming',
+          time: Map({
+            hour: 7,
+            day: 1,
+            week: 1,
+            phase: 1
+          }),
+          resources: Map({
+            energy: 500,
+            mood: 100
+          }),
+          skills: Map({
+            softSkills: 0,
+            techSkills: 0
+          }),
+          tasks: List([
+            Map({
+              name: 'Pair Programming',
+              resources: Map({
+                energy: -10,
+                mood: -3
+              }),
+              skills: Map({
+                softSkills: 3,
+                techSkills: 1
+              }),
+              initalCosts: Map({
+                energy: 10,
+                mood: 3
+              })
+            })
+            ])
+        })
         const nextState = reducer(initialState, action)
         expect(nextState.getIn(['time', 'hour'])).to.equal(8)
       })
       it('resets the hour to 7 am if the time is over 9pm', () => {
-        const initialState = Map({time: Map({hour: 22, day: 1, week: 1,
-         phase: 1 })})
+        const initialState = Map({
+          activeTask: 'Pair Programming',
+          time: Map({
+            hour: 22,
+            day: 1,
+            week: 1,
+            phase: 1
+          }),
+          resources: Map({
+            energy: 500,
+            mood: 100
+          }),
+          skills: Map({
+            softSkills: 0,
+            techSkills: 0
+          }),
+          tasks: List([
+            Map({
+              name: 'Pair Programming',
+              resources: Map({
+                energy: -10,
+                mood: -3
+              }),
+              skills: Map({
+                softSkills: 3,
+                techSkills: 1
+              }),
+              initalCosts: Map({
+                energy: 10,
+                mood: 3
+              })
+            })
+            ])
+        })
         const nextState = reducer(initialState, action)
         expect(nextState.getIn(['time', 'hour'])).to.equal(7)
         expect(nextState.getIn(['time', 'day'])).to.equal(2)
@@ -35,15 +131,79 @@ describe('reducer', () => {
     })
     describe('days', () => {
       it('increases the day', () => {
-        const initialState = Map({time: Map({hour: 21, day: 1, week: 1, 
-          phase: 1})})
+        const initialState = Map({
+          activeTask: 'Pair Programming',
+          time: Map({
+            hour: 21,
+            day: 1,
+            week: 1,
+            phase: 1
+          }),
+          resources: Map({
+            energy: 500,
+            mood: 100
+          }),
+          skills: Map({
+            softSkills: 0,
+            techSkills: 0
+          }),
+          tasks: List([
+            Map({
+              name: 'Pair Programming',
+              resources: Map({
+                energy: -10,
+                mood: -3
+              }),
+              skills: Map({
+                softSkills: 3,
+                techSkills: 1
+              }),
+              initalCosts: Map({
+                energy: 10,
+                mood: 3
+              })
+            })
+            ])
+        })
         const nextState = reducer(initialState, action)
         expect(nextState.getIn(['time', 'hour'])).to.equal(7)
         expect(nextState.getIn(['time', 'day'])).to.equal(2)
       })
       it('resets the day to 1 if the day is over 5', () => {
-        const initialState = Map({time: Map({hour: 22, day: 5, week: 1, 
-          phase: 1})})
+        const initialState = Map({
+          activeTask: 'Pair Programming',
+          time: Map({
+            hour: 21,
+            day: 5,
+            week: 1,
+            phase: 1
+          }),
+          resources: Map({
+            energy: 500,
+            mood: 100
+          }),
+          skills: Map({
+            softSkills: 0,
+            techSkills: 0
+          }),
+          tasks: List([
+            Map({
+              name: 'Pair Programming',
+              resources: Map({
+                energy: -10,
+                mood: -3
+              }),
+              skills: Map({
+                softSkills: 3,
+                techSkills: 1
+              }),
+              initalCosts: Map({
+                energy: 10,
+                mood: 3
+              })
+            })
+            ])
+        })
         const nextState = reducer(initialState, action)
         expect(nextState.getIn(['time', 'day'])).to.equal(1)
         expect(nextState.getIn(['time', 'week'])).to.equal(2)
@@ -51,25 +211,120 @@ describe('reducer', () => {
     })
     describe('weeks', () => {
       it('increase the week', () => {
-        const initialState = Map({time: Map({hour: 22, day: 5, week: 1, 
-          phase: 1})
-      })
+        const initialState = Map({
+          activeTask: 'Pair Programming',
+          time: Map({
+            hour: 21,
+            day: 5,
+            week: 1,
+            phase: 1
+          }),
+          resources: Map({
+            energy: 500,
+            mood: 100
+          }),
+          skills: Map({
+            softSkills: 0,
+            techSkills: 0
+          }),
+          tasks: List([
+            Map({
+              name: 'Pair Programming',
+              resources: Map({
+                energy: -10,
+                mood: -3
+              }),
+              skills: Map({
+                softSkills: 3,
+                techSkills: 1
+              }),
+              initalCosts: Map({
+                energy: 10,
+                mood: 3
+              })
+            })
+            ])
+        })
         const nextState = reducer(initialState, action)
         expect(nextState.getIn(['time', 'hour'])).to.equal(7)
         expect(nextState.getIn(['time', 'day'])).to.equal(1)
         expect(nextState.getIn(['time', 'week'])).to.equal(2)
       })
       it('correctly resets the week to 1 if greater than 3', () => {
-        const initialState = Map({time: Map({hour: 22, day: 5,week: 3, 
-          phase: 1})})
+        const initialState = Map({
+          activeTask: 'Pair Programming',
+          time: Map({
+            hour: 21,
+            day: 5,
+            week: 3,
+            phase: 1
+          }),
+          resources: Map({
+            energy: 500,
+            mood: 100
+          }),
+          skills: Map({
+            softSkills: 0,
+            techSkills: 0
+          }),
+          tasks: List([
+            Map({
+              name: 'Pair Programming',
+              resources: Map({
+                energy: -10,
+                mood: -3
+              }),
+              skills: Map({
+                softSkills: 3,
+                techSkills: 1
+              }),
+              initalCosts: Map({
+                energy: 10,
+                mood: 3
+              })
+            })
+            ])
+        })
         const nextState = reducer(initialState, action)
         expect(nextState.getIn(['time', 'week'])).to.equal(1)
       })
     })
     describe('phases', () => {
       it('increases the phase', () => {
-        const initialState = Map({time: Map({hour: 21, day: 5, week: 3, 
-          phase: 1})})
+        const initialState = Map({
+          activeTask: 'Pair Programming',
+          time: Map({
+            hour: 21,
+            day: 5,
+            week: 3,
+            phase: 1
+          }),
+          resources: Map({
+            energy: 500,
+            mood: 100
+          }),
+          skills: Map({
+            softSkills: 0,
+            techSkills: 0
+          }),
+          tasks: List([
+            Map({
+              name: 'Pair Programming',
+              resources: Map({
+                energy: -10,
+                mood: -3
+              }),
+              skills: Map({
+                softSkills: 3,
+                techSkills: 1
+              }),
+              initalCosts: Map({
+                energy: 10,
+                mood: 3
+              })
+            })
+            ])
+        })
         const nextState = reducer(initialState, action)
         expect(nextState.getIn(['time', 'hour'])).to.equal(7)
         expect(nextState.getIn(['time', 'day'])).to.equal(1)
@@ -77,144 +332,176 @@ describe('reducer', () => {
         expect(nextState.getIn(['time', 'phase'])).to.equal(2)
       })
       it('ends the game', () => {
-        const initialState = Map({time: Map({hour: 21, day: 5, week: 3,
-          phase: 3})})
-        const nextState = reducer(initialState, action)
-        expect(nextState.get('gameover')).to.be.true
+       const initialState = Map({
+        activeTask: 'Pair Programming',
+        time: Map({
+          hour: 21,
+          day: 5,
+          week: 3,
+          phase: 3
+        }),
+        resources: Map({
+          energy: 500,
+          mood: 100
+        }),
+        skills: Map({
+          softSkills: 0,
+          techSkills: 0
+        }),
+        tasks: List([
+          Map({
+            name: 'Pair Programming',
+            resources: Map({
+              energy: -10,
+              mood: -3
+            }),
+            skills: Map({
+              softSkills: 3,
+              techSkills: 1
+            }),
+            initalCosts: Map({
+              energy: 10,
+              mood: 3
+            })
+          })
+          ])
       })
+       const nextState = reducer(initialState, action)
+       expect(nextState.get('gameover')).to.be.true
+     })
     })
   })
-  describe('ACTIVATE_TASK', () => {
-    const initialState = Map({
-      activeTask: '',
-      time: Map({
-        hour: 7,
-        day: 1,
-        week: 1,
-        phase: 1
-      }),
-      resources: Map({
-        energy: 100,
-        mood: 100
-      }),
-      skills: Map({
-        softSkills: 0,
-        techSkills: 0
-      }),
-      tasks: List([
-        Map({
-          name: 'Pair Programming',
-          resources: Map({
-            energy: -10,
-            mood: -3
-          }),
-          skills: Map({
-            softSkills: 3,
-            techSkills: 1
-          }),
-          initalCosts: Map({
-            energy: 30,
-            mood: 10
-          })
+describe('ACTIVATE_TASK', () => {
+  const initialState = Map({
+    activeTask: '',
+    time: Map({
+      hour: 7,
+      day: 1,
+      week: 1,
+      phase: 1
+    }),
+    resources: Map({
+      energy: 100,
+      mood: 100
+    }),
+    skills: Map({
+      softSkills: 0,
+      techSkills: 0
+    }),
+    tasks: List([
+      Map({
+        name: 'Pair Programming',
+        resources: Map({
+          energy: -10,
+          mood: -3
+        }),
+        skills: Map({
+          softSkills: 3,
+          techSkills: 1
+        }),
+        initalCosts: Map({
+          energy: 30,
+          mood: 10
         })
-        ])
-    })
-    const initialStateNoResources = Map({
-      activeTask: '',
-      time: Map({
-        hour: 7,
-        day: 1,
-        week: 1,
-        phase: 1
-      }),
-      resources: Map({
-        energy: 0,
-        mood: 100
-      }),
-      skills: Map({
-        softSkills: 0,
-        techSkills: 0
-      }),
-      tasks: List([
-        Map({
-          name: 'Pair Programming',
-          resources: Map({
-            energy: -10,
-            mood: -3
-          }),
-          skills: Map({
-            softSkills: 3,
-            techSkills: 1
-          }),
-          initalCosts: Map({
-            energy: 10,
-            mood: 3
-          })
+      })
+      ])
+  })
+  const initialStateNoResources = Map({
+    activeTask: 'Pair Programming',
+    time: Map({
+      hour: 7,
+      day: 1,
+      week: 1,
+      phase: 1
+    }),
+    resources: Map({
+      energy: 0,
+      mood: 100
+    }),
+    skills: Map({
+      softSkills: 0,
+      techSkills: 0
+    }),
+    tasks: List([
+      Map({
+        name: 'Pair Programming',
+        resources: Map({
+          energy: -10,
+          mood: -3
+        }),
+        skills: Map({
+          softSkills: 3,
+          techSkills: 1
+        }),
+        initalCosts: Map({
+          energy: 10,
+          mood: 3
         })
-        ])
-    })
+      })
+      ])
+  })
 
-    const initialStateActive = Map({
-      activeTask: 'Pair Programming',
-      time: Map({
-        hour: 7,
-        day: 1,
-        week: 1,
-        phase: 1
-      }),
-      resources: Map({
-        energy: 100,
-        mood: 100
-      }),
-      skills: Map({
-        softSkills: 0,
-        techSkills: 0
-      }),
-      tasks: List([
-        Map({
-          name: 'Pair Programming',
-          resources: Map({
-            energy: -10,
-            mood: -3
-          }),
-          skills: Map({
-            softSkills: 3,
-            techSkills: 1
-          }),
-          initalCosts: Map({
-            energy: 10,
-            mood: 3
-          })
+  const initialStateActive = Map({
+    activeTask: 'Pair Programming',
+    time: Map({
+      hour: 7,
+      day: 1,
+      week: 1,
+      phase: 1
+    }),
+    resources: Map({
+      energy: 100,
+      mood: 100
+    }),
+    skills: Map({
+      softSkills: 0,
+      techSkills: 0
+    }),
+    tasks: List([
+      Map({
+        name: 'Pair Programming',
+        resources: Map({
+          energy: -10,
+          mood: -3
+        }),
+        skills: Map({
+          softSkills: 3,
+          techSkills: 1
+        }),
+        initalCosts: Map({
+          energy: 10,
+          mood: 3
         })
-        ])
-    })
-    const action = {
-      type: 'ACTIVATE_TASK',
-      task: 'Pair Programming'
-    }
-    const nextState = reducer(initialState, action)
-    const nextStateNoResources = reducer(initialStateNoResources, action)
-    const nextStateActive = reducer(initialStateActive, action)
+      })
+      ])
+  })
+  const action = {
+    type: 'ACTIVATE_TASK',
+    task: 'Pair Programming'
+  }
+  const nextState = reducer(initialState, action)
+  const nextStateNoResources = reducer(initialStateNoResources, action)
+  const nextStateActive = reducer(initialStateActive, action)
 
-    describe('Activates the task', () => {
-      it('sets the active task to be the specified task', () => {
-        expect(nextState.get('activeTask')).to.equal('Pair Programming')
-      })
-      it('doesn not change task if task is already active', () => {
-        expect(nextStateActive.get('activeTask')).to.equal('Pair Programming')
-        expect(nextStateActive.getIn(['resources', 'energy'])).to.equal(100)
-        expect(nextStateActive.getIn(['resources', 'mood'])).to.equal(100)
-      })
+  describe('Activates the task', () => {
+    it('sets the active task to be the specified task', () => {
+      expect(nextState.get('activeTask')).to.equal('Pair Programming')
     })
-    describe('Depletes resouces', () => {
-      it('Depletes resources by inital cost of task', () => {
-        expect(nextState.getIn(['resources', 'energy'])).to.equal(70)
-        expect(nextState.getIn(['resources', 'mood'])).to.equal(90)
-      })
-      it('Does not deplete resources if not enough resources to ask task', () => {
-        expect(nextStateNoResources.getIn(['resources', 'energy'])).to.equal(0)
-        expect(nextStateNoResources.getIn(['resources', 'mood'])).to.equal(100)
-      })
+    it('doesn not change task if task is already active', () => {
+      expect(nextStateActive.get('activeTask')).to.equal('Pair Programming')
+      expect(nextStateActive.getIn(['resources', 'energy'])).to.equal(100)
+      expect(nextStateActive.getIn(['resources', 'mood'])).to.equal(100)
     })
   })
+  describe('Depletes resouces', () => {
+    it('Depletes resources by inital cost of task', () => {
+      expect(nextState.getIn(['resources', 'energy'])).to.equal(70)
+      expect(nextState.getIn(['resources', 'mood'])).to.equal(90)
+    })
+    it('Does not deplete resources if not enough resources to ask task', () => {
+      expect(nextStateNoResources.getIn(['resources', 'energy'])).to.equal(0)
+      expect(nextStateNoResources.getIn(['resources', 'mood'])).to.equal(100)
+    })
+  })
+})
 })
