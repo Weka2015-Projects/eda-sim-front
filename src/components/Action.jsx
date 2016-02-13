ea5bb57958685f4590deb6241272a581202d371d
Irene Robb <IreneRobb@Irenes-MacBook-Pro.local>
2016-02-14 12:23:54 +1300

    adding cost test in action

--- src/components/Action.jsx
+++ src/components/Action.jsx
@@ -9,10 +9,24 @@ export class Action extends Component {
     super(props)
 
   }
+
   render() {
+    console.log(this.props.resources)
+    const skillsArray = this.props.skills.map((s, index) => {
+      return <div className="skill">{index}: +{s}</div>
+    })
+    const resourcesArray = this.props.resources.map((r, index) => {
+
+      return <div className="resource">{index}: {r}</div>
+    })
     return (
       <div className="action">
         <h6>{this.props.name}</h6>
+        <div className="action-details">
+          <div className="skills">{skillsArray}</div>
+          <div className="cost">{resourcesArray}</div>
+          <div className="initial-cost"></div>
+        </div>
       </div>
     )
   }
--- test/components/action-spec.js
+++ test/components/action-spec.js
@@ -9,17 +9,29 @@ import { mount, render, shallow } from 'enzyme'
 import {Action} from '../../src/components/Action.jsx'
 
 describe('<Action/>', () => {
-  it('renders without any props', () => {
-    const wrapper = shallow(<Action/>)
-    expect(wrapper).to.be.ok
+  const skills = Map({
+      softSkills: 3,
+      techSkills: 1
+    })
+  const resources = Map({
+    energy: -10,
+    mood: -3
   })
   it('has a classname of resource', () => {
-    const wrapper = shallow(<Action/>)
+    const wrapper = shallow(<Action skills={skills} resources={resources}/>)
     expect(wrapper).to.have.className('action')
   })
   it('renders task name', () => {
-    const wrapper = shallow(<Action name='pair programming'/>)
+    const wrapper = shallow(<Action name='pair programming' skills={skills} resources={resources}/>)
     expect(wrapper.find('h6')).to.have.text('pair programming')
+  })
+  it('displays the correct skills', () => {
+    const wrapper = shallow(<Action name='pair programming' skills={skills} resources={resources}/>)
+    expect(wrapper.find('.action-details').find('.skills').find('.skill').first()).to.have.text('softSkills: +3')
+  })
+  it('displays the correct costs', () => {
+    const wrapper = shallow(<Action name='pair programming' skills={skills} resources={resources}/>)
+    expect(wrapper.find('.action-details').find('.cost').find('.resource').first()).to.have.text('energy: -10')
   })
 })
 
