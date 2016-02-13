8608947eed2df559d6298936a67f09aff02b0016
NiluK <nkulasingham@gmail.com>
2016-02-13 19:29:43 +1300

    commented out uncesscary test code instead of deleting it

--- test/components/items-spec.js
+++ test/components/items-spec.js
@@ -1,19 +1,19 @@
-import React from 'react'
-import chai, { expect } from 'chai'
-import chaiEnzyme from 'chai-enzyme'
+// import React from 'react'
+// import chai, { expect } from 'chai'
+// import chaiEnzyme from 'chai-enzyme'
 
-chai.use(chaiEnzyme())
+// chai.use(chaiEnzyme())
 
-import { mount, render, shallow } from 'enzyme'
-import {Items} from '../../src/components/Items.jsx'
+// import { mount, render, shallow } from 'enzyme'
+// import {Items} from '../../src/components/Items.jsx'
 
-describe('<Items />', () => {
-  it('renders without any props', () => {
-    const wrapper = shallow(<Items/>)
-    expect(wrapper).to.be.ok
-  })
-  it('has a classname of items', () => {
-    const wrapper = shallow(<Items />)
-    expect(wrapper).to.have.className('items')
-  })
-})
+// describe('<Items />', () => {
+//   it('renders without any props', () => {
+//     const wrapper = shallow(<Items/>)
+//     expect(wrapper).to.be.ok
+//   })
+//   it('has a classname of items', () => {
+//     const wrapper = shallow(<Items />)
+//     expect(wrapper).to.have.className('items')
+//   })
+// })
