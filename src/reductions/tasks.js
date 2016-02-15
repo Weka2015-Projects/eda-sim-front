d67869e205634bbd5246400d638c15174851b4b5
Joshua Vial <joshua@enspiral.com>
2016-02-15 17:53:37 +1300

    review

--- src/reductions/tasks.js
+++ src/reductions/tasks.js
@@ -9,22 +9,27 @@ function hasEnoughResources(state, task){
 
 function activateTask(state, task) {
   if (task === state.get('activeTask')) return state
-    const taskId = state.toJS().tasks.findIndex(
-      (indexes) => indexes.name === task)
+
+  const taskId = state.toJS().tasks.findIndex(
+    (indexes) => indexes.name === task)
+
   return hasEnoughResources(state, taskId) ?
   depleteResources(state, task, taskId) : state
 }
 
 function continueTask (state, task) {
   if (task === '') return state
-    const taskId = state.toJS().tasks.findIndex(
-      (indexes) => indexes.name === task)
+
+  const taskId = state.toJS().tasks.findIndex(
+    (indexes) => indexes.name === task)
+
   return undergoTask(state, task, taskId)
 }
 
 function depleteResources (state, task, taskId) {
   let newState = state
   const costs = state.toJS().tasks[taskId].initialCosts
+
   for (let i =0; i < Object.keys(costs).length;  i++) {
     const cost = costs[Object.keys(costs)[i]]
     const resource = state.toJS().resources[Object.keys(costs)[i]]
@@ -36,13 +41,20 @@ function depleteResources (state, task, taskId) {
   return newState
 }
 
+//too long
 function undergoTask(state, task, taskId) {
+  //maybe something like
+  //if (!enoughResources(state, task)) {return state}
+  //let newState = updateResources(state)
+  //return updateSkills(newState)
+  
   let newState = state
   const costs = state.toJS().tasks[taskId].resources
   const resources = state.toJS().resources
   const sortedKeys = Object.keys(resources).sort((a, b) => {
     return resources[a] - resources[b]
   }).filter((value) => Object.keys(costs).indexOf(value) !== -1)
+
   for (let i =0; i < Object.keys(costs).length;  i++) {
     const cost = costs[sortedKeys[i]]
     const resource = state.toJS().resources[sortedKeys[i]]
@@ -59,8 +71,6 @@ function undergoTask(state, task, taskId) {
     const skill = state.toJS().skills[Object.keys(gains)[i]]
     newState = newState.updateIn(['skills', Object.keys(gains)[i], 'exp'],
       (skill) => skill+gain)
-  }
-  for (let i =0; i < Object.keys(gains).length;  i++) {
     const exp = newState.toJS().skills[Object.keys(gains)[i]].exp
     const expToLevel = newState.toJS().skills[Object.keys(gains)[i]].expToLevel
     const levelIncrement = Math.floor((exp /expToLevel))
