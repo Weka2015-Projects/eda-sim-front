3e236d48f730876429c3510e2f6b4c33c21fed78
Irene Robb <IreneRobb@Irenes-MacBook-Pro.local>
2016-02-17 14:04:06 +1300

    swissbaaaaaalll

--- src/components/DancingMan.jsx
+++ src/components/DancingMan.jsx
@@ -13,18 +13,19 @@ export class DancingMan extends Component {
     return (
       <div className="stickMan">
         <svg height="2000" width="2000">
-          <g transform="translate(200,55) rotate(10)">
-            <line id="armOne" x1="20" y1="90" x2="100" y2="20" stroke="black" strokeWidth="4"/>
-            <line id="armTwo" x1="200" y1="90" x2="100" y2="20" stroke="black" strokeWidth="4"/>
-            <line id="torso" x1="130" y1="100" x2="100" y2="20" stroke="black" strokeWidth="4"/>
+          <g transform="translate(20,55) rotate(10)">
+            <line id="armOne" x1="20" y1="90" x2="100" y2="20" stroke="black" strokeWidth="3"/>
+            <line id="armTwo" x1="200" y1="90" x2="100" y2="20" stroke="black" strokeWidth="3"/>
+            <line id="torso" x1="130" y1="100" x2="100" y2="20" stroke="black" strokeWidth="3"/>
           </g>
-          <circle id="face" cx="300" cy="60" r="30" fill="white" stroke="black" strokeWidth="4"/>
-          <circle id="eyeOne" cx="280" cy="60" r="5" fill="white" stroke="black" strokeWidth="4"/>
-          <circle id="eyeTwo" cx="310" cy="60" r="5" fill="white" stroke="black" strokeWidth="4"/>
-          <g transform="translate(200,140) rotate(10)">
-            <line id="legOne" x1="20" y1="90" x2="110" y2="20" stroke="black" strokeWidth="4"/>
-            <line id="legTwo" x1="200" y1="90" x2="110" y2="20" stroke="black" strokeWidth="4"/>
+          <circle id="face" cx="120" cy="60" r="30" fill="white" stroke="black" strokeWidth="3"/>
+          <circle id="eyeOne" cx="100" cy="60" r="5" fill="white" stroke="black" strokeWidth="3"/>
+          <circle id="eyeTwo" cx="130" cy="60" r="5" fill="white" stroke="black" strokeWidth="3"/>
+          <g transform="translate(25,135) rotate(10)">
+            <line id="legOne" x1="20" y1="90" x2="110" y2="20" stroke="black" strokeWidth="3"/>
+            <line id="legTwo" x1="200" y1="90" x2="110" y2="20" stroke="black" strokeWidth="3"/>
           </g>
+          <circle id="swissball" cx="400" cy="200" r="80" fill="white" stroke="black" fill="#3399ff" strokeWidth="1"/>
         </svg>
       </div>
     )
